bbda0e618381a2bb094d93bc09463b551943db2b
Laby Damaro Camara <ldamaro98@gmail.com>
2019-02-02 17:55:37 +0100

    Utilisation ListView.builder

--- flutter_learn/lib/main.dart
+++ flutter_learn/lib/main.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
-String src = 'https://shoesyourpath.com/wp-content/uploads/2015/12/cascades-kuang-si-luang-prabang-laos.jpg';
-var assetsImage = AssetImage('images/laos.jpg');
-var image = Image(image:assetsImage,fit: BoxFit.cover,);
+
+List<String> content = [
+  'one',
+  'two',
+  'three',
+  '4',
+  'one',
+  'two',
+  'three',
+  '4',
+  'one',
+  'two',
+  'three',
+  '4',
+  'one',
+  'two',
+  'three',
+  '4',
+  'one',
+  'two',
+  'three',
+  '4',
+];
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Dieu est grand !!!',
+      title: 'Dieu est grand',
       home: Home(),
     );
   }
@@ -16,52 +37,40 @@ class MyApp extends StatelessWidget {
 
 class Home extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
+      
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        onPressed: (){print('Floatting button action');},
         child: Icon(Icons.add),
-        backgroundColor: Colors.black,
-        onPressed:(){print('You press the flotting button Add!');}
       ),
-      appBar: AppBar(
+      appBar:AppBar(
         backgroundColor: Colors.black,
-        title: Text(
-          'Gloire à Dieu !!!',
-        ),
-        centerTitle:true,
+        title:Text('Dieu est grand'),
+        centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.settings),
-          onPressed:(){print('You press the button Settings!');}
+          onPressed: (){print('Settings');},
         ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
-            onPressed:(){print('You press the button Search!');}
+            onPressed: (){print('Search');},
           ),
           IconButton(
             icon: Icon(Icons.menu),
-            onPressed:(){print('You press the button Menu!');}
-          ),
+            onPressed: (){print('Menu');},
+          )
         ],
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.map),
-            title:Text('Map'),
-            onTap: (){print('Map');},
-          ),
-          ListTile(
-            leading:  Icon(Icons.search),
-            title:Text('Search'),
-            onTap:(){print('Search');}
-          ),
-          ListTile(
-            leading: Icon(Icons.select_all),
-            title: Text('Select All'),
-            onTap: (){print('Select all');}
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: content.length,
+        itemBuilder: (BuildContext context, int index){
+          return ListTile(
+            title: Text(content[index]),
+          );
+        },
       ),
     );
   }
