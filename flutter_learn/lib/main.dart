42c1f89c3901f3529df12052fefc50f8b9208785
Laby Damaro Camara <ldamaro98@gmail.com>
2019-02-02 15:11:12 +0100

    Utilisation de assetImage

--- flutter_learn/lib/main.dart
+++ flutter_learn/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-
+String src = 'https://shoesyourpath.com/wp-content/uploads/2015/12/cascades-kuang-si-luang-prabang-laos.jpg';
+var assetsImage = AssetImage('images/laos.jpg');
+var image = Image(image:assetsImage,fit: BoxFit.cover,);
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dieu est grand!!!',
+      title: 'Dieu est grand !!!',
       home: Home(),
     );
   }
@@ -16,50 +18,43 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     floatingActionButton: FloatingActionButton(
-       onPressed: (){print('Floatting Action Button');},
-       child: Icon(Icons.add),
-       backgroundColor: Colors.black,
-     ),
-     appBar: AppBar(
-       backgroundColor: Colors.black,
-       title: Text(
-         'Gloire à Dieu !!!',  
-       ),
-       centerTitle: true,
-       leading: IconButton(
-         icon:Icon(Icons.settings),
-         onPressed:(){print('Settings');}
-       ),
-       actions: <Widget>[
-         IconButton(
-           icon: Icon(Icons.search),
-           onPressed:(){print('Search');}
-         ),
-         IconButton(
-           icon: Icon(Icons.menu),
-           onPressed:(){print('Menu');}
-         ),
-       ]
-     ),
-     body: Center(
-       child: Container(
-         constraints: BoxConstraints.expand(width: 300),
-         decoration: BoxDecoration(
-           color: Colors.lightBlue,
-           border: Border.all(width: 3.0), 
-           borderRadius: BorderRadius.circular(15),  
-         ),
-         child: Text(
-           'Bonjour Camara Laby Damaro !!!',
-           style: TextStyle(
-             fontFamily: 'Verdana',
-             fontSize: 20.0,
-             fontWeight: FontWeight.w800,
-           )
-         ),
-       )
-     ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.black,
+        onPressed:(){print('You press the flotting button Add!');}
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Gloire à Dieu !!!',
+        ),
+        centerTitle:true,
+        leading: IconButton(
+          icon: Icon(Icons.settings),
+          onPressed:(){print('You press the button Settings!');}
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed:(){print('You press the button Search!');}
+          ),
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed:(){print('You press the button Menu!');}
+          ),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          width: 300.0,
+          height: 250.0,
+          decoration: BoxDecoration(
+            border: Border.all(width: 2.0),
+
+          ),
+          child: image,
+        ),
+      ),
     );
   }
 }
